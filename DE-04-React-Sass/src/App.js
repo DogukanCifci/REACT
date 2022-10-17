33f370bf9042fd90395fc105f5081422c9644105
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-10-17 11:33:07 +0200

    cards structure done

--- DE-04-React-Sass/src/App.js
+++ DE-04-React-Sass/src/App.js
@@ -1,10 +1,14 @@
 import "./App.scss";
+import "./components/cards/Card.scss";
+import Mycard from "./components/cards/Mycard";
 import Myheader from "./components/header/Myheader";
+import dataa from "./data";
 
 function App() {
   return (
     <div>
       <Myheader />
+      <Mycard data1={dataa} />
     </div>
   );
 }
---
+++ DE-04-React-Sass/src/components/cards/Mycard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Card.scss";
+//keyword olarak verdigim kelimeyi dalgali parantez icinde yazmaliyim. Eger direk liste halinde almak istiyorsam. Diger türlü props olarak da alabilirim ama fafrki console.log icinde yazdirarak görebilrisiniz.
+const Mycard = ({ data1 }) => {
+  console.log(data1);
+  return (
+    <div>
+      <div className="container">
+        {data1.map((e) => {
+          const { id, name, job, comment, img } = e;
+          return (
+            <div key={id} className="cards">
+              <h1>{name}</h1>
+              <h2>{job}</h2>
+              <p>{comment}</p>
+              <img src={img} alt="" />
+
+              <div className="btn-container">
+                <button className="btn-container-small">Small</button>
+                <button className="btn.container--large">Large</button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+export default Mycard;
