f5531b204adf9722d9ee4df8b4d49c0c66d2a34b
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-10-19 11:11:14 +0200

    object with useState() added

--- DE-05-Events-Hooks/src/2-hooks/Hooks.jsx
+++ DE-05-Events-Hooks/src/2-hooks/Hooks.jsx
@@ -3,31 +3,60 @@ const Hooks = () => {
   //setSayac yerine istedigimiz herhangi bir kelime yazabiliriz. Ama ilk kelimenin basina set getirilir bestpractise'de
   //usestate icinde yxazilan deger degiskenin ilk degeri anlaminda. Ayrica useState icine string ifade de yazilabilir
 
+  //Bu const en üstte tanimlanmasi daha iyi Cünkü alttakilerin calismasinda sikinti olmamasi icin
   const [sayac, setSayac] = useState(0);
+
+  const [infos, setInfos] = useState({
+    isim: "Mevlut",
+    email: "Mevlüt@gmail.com",
+    age: 26,
+  });
+
   const artir = () => {
     setSayac(sayac + 1);
     //Burda setSayac bizim ayakcimiuz. Istedigimiz degisikligi ayakcinin parantezi icinde orijinal degiskenle yapiyoruz. document.qeruy ile tekrar textContexti yakalamamiza gerek kalmiyor. Bu sekilde otomatik olarak güncelleniyor.
   };
 
-  const azalt = () => {};
+  const azalt = () => {
+    setSayac(sayac - 1);
+  };
 
-  const temizle = () => {};
+  const temizle = () => {
+    setSayac(0);
+  };
+
+  const changing = () => {
+    setInfos({ isim: "Muhammed", email: "Muhammed@gmail.com", age: 29 });
+  };
   return (
     <div>
-      <div className="container">
-        <h2> **********************************************************</h2>
+      <div className="container-fluid">
+        <h2>*****************************************************</h2>
+        <div className="container">
+          <h1>USESTATE</h1>
+          <h2>COUNT : {sayac}</h2>
+          <button onClick={artir} className="btn btn-primary">
+            ARTIR
+          </button>
+          <button onClick={azalt} className="btn btn-danger">
+            AZALT
+          </button>
+          <button className="btn btn-warning" onClick={temizle}>
+            TEMIZLE
+          </button>
+        </div>
 
-        <h1>USESTATE</h1>
-        <h2>COUNT : {sayac}</h2>
-        <button onClick={artir} className="btn btn-primary">
-          ARTIR
-        </button>
-        <button onClick={azalt} className="btn btn-danger">
-          AZALT
-        </button>
-        <button className="btn btn-warning" onClick={temizle}>
-          TEMIZLE
-        </button>
+        <h1>*****************************************************</h1>
+
+        <div className="text-center mt-5">
+          <h2 className="mb-3 text-success">UseStatein Objectte Kullanimi</h2>
+          <h5>{infos.isim}</h5>
+          <h5>{infos.email}</h5>
+          <h5>{infos.age}</h5>
+          <button className="btn btn-success" onClick={changing}>
+            DEGISTIR
+          </button>
+        </div>
       </div>
     </div>
   );
