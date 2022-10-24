17ecb6bf4a74998ee1c8f16a1f1fda9b599cc76b
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-10-24 23:38:58 +0200

    to create new users with faker added

--- DE-07-UseEffect/src/components/PeopleAxios.jsx
+++ DE-07-UseEffect/src/components/PeopleAxios.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import faker from "faker";
 const PeopleAxios = () => {
   const [insanlar, setInsanlar] = useState([]);
 
@@ -16,14 +17,19 @@ const PeopleAxios = () => {
   const postData = () => {
     /* ... adresine , den sonraki veriyi yolladik ve daha sonra da o veriyi set metoduyla degistirdik. Ama bir önceki array#a ekleyerek gitmek icin spread metdounu kullaniyrouz. Bir önceki listeyi temsil eden insanlar degiskenini ... ile acip , koyduktan sonra yeni ekledigimiz degeri ekliyoruz.   */
     axios
-      .post("https://jsonplaceholder.typicode.com/users", {
-        name: "Sultan",
-        phone: 12345678,
-      })
-      .then((res) => setInsanlar([...insanlar, res.data]));
+      .post("https://jsonplaceholder.typicode.com/users", { veri })
+      .then((res) => setInsanlar([...insanlar, res.data.veri]));
   };
-  // console.log(insanlar);
+  console.log(insanlar);
+  //Yukarda res.data.veri yazmamin sebebi; veri ismiyle bir object olusturdugum icin key veri oluyor ve value bu object icindeki key:value pairleri oluyor. Benim de data'ya eklemek istedigim asil bilgiler id,name,phone oldugu icin önce veri keyine ulasiyorum.
 
+  const veri = {
+    id: insanlar.length + 1,
+    name: faker.name.findName(),
+    phone: faker.phone.phoneNumber(),
+  };
+
+  console.log(veri); //Veri her tikladigimda bir sonraki veri olusut ama post'a bastigimda eklenir ve bir sonraki veri otomatik olusur.Yani posta basmadan aslinda veri olusuyor. Bastiktan sonra o veri ekleniyor ve yeni bir veri otomatik olarak olusuyor.Yani önce veri olusuyor eger post butonuna basarsam data arraya'ine eklenir.
   return (
     <div className="container text-center mt-4">
       <button
