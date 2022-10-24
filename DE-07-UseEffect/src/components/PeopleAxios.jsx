9c9ba86ed6bd19691422aacde7a9d628bdf670f6
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-10-24 23:08:14 +0200

    axios post method added

--- DE-07-UseEffect/src/components/PeopleAxios.jsx
+++ DE-07-UseEffect/src/components/PeopleAxios.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 const PeopleAxios = () => {
-  const [insanlar, setInsanlar] = useState();
+  const [insanlar, setInsanlar] = useState([]);
 
   useEffect(() => {
     axios
@@ -12,9 +12,27 @@ const PeopleAxios = () => {
   //setInsanlar icine response.data yazmamzin sebebi response olarak geleni console.log ile yazdirirsak icinde bir süreü gereksiz bilgiler de beraber geliyor. Bize lazim olan array ise data keyinin icinde. Onun icin icinden datayi cekip aldik.
 
   //catch i istege bagli olarak yaptim. Yapmayabilirdim. Catch'i istersem normal fetch'de de kullanabilirim.
-  console.log(insanlar);
+
+  const postData = () => {
+    /* ... adresine , den sonraki veriyi yolladik ve daha sonra da o veriyi set metoduyla degistirdik. Ama bir önceki array#a ekleyerek gitmek icin spread metdounu kullaniyrouz. Bir önceki listeyi temsil eden insanlar degiskenini ... ile acip , koyduktan sonra yeni ekledigimiz degeri ekliyoruz.   */
+    axios
+      .post("https://jsonplaceholder.typicode.com/users", {
+        name: "Sultan",
+        phone: 12345678,
+      })
+      .then((res) => setInsanlar([...insanlar, res.data]));
+  };
+  // console.log(insanlar);
+
   return (
     <div className="container text-center mt-4">
+      <button
+        className="btn btn-success"
+        onClick={postData}
+        style={{ fontSize: "1rem" }}
+      >
+        Post Data
+      </button>
       <div className="row">
         {insanlar &&
           insanlar.map((e) => {
