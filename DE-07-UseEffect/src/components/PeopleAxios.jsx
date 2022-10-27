3fab217ccfdf578065441a23ba73ea0b39a8b3b5
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-10-27 09:03:11 +0200

    new Topic and pages added

--- DE-07-UseEffect/src/components/PeopleAxios.jsx
+++ DE-07-UseEffect/src/components/PeopleAxios.jsx
@@ -17,19 +17,21 @@ const PeopleAxios = () => {
   const postData = () => {
     /* ... adresine , den sonraki veriyi yolladik ve daha sonra da o veriyi set metoduyla degistirdik. Ama bir önceki array#a ekleyerek gitmek icin spread metdounu kullaniyrouz. Bir önceki listeyi temsil eden insanlar degiskenini ... ile acip , koyduktan sonra yeni ekledigimiz degeri ekliyoruz.   */
     axios
-      .post("https://jsonplaceholder.typicode.com/users", { veri })
-      .then((res) => setInsanlar([...insanlar, res.data.veri]));
+      .post("https://jsonplaceholder.typicode.com/users", { myVeri })
+      .then((res) => setInsanlar([...insanlar, res.data.myVeri]));
   };
   console.log(insanlar);
-  //Yukarda res.data.veri yazmamin sebebi; veri ismiyle bir object olusturdugum icin key veri oluyor ve value bu object icindeki key:value pairleri oluyor. Benim de data'ya eklemek istedigim asil bilgiler id,name,phone oldugu icin önce veri keyine ulasiyorum.
+  //Yukarda res.data.veri yazmamin sebebi; veri ismiyle bir object olusturdugum icin key myVeri oluyor ve value bu object icindeki key:value pairleri oluyor. Benim de data'ya eklemek istedigim asil bilgiler id,name,phone oldugu icin önce myVeri key'ine ulasiyorum.
 
-  const veri = {
+  //!!Normalde veriyi direk yukarda post isleminden sonraki {veri} yerine de yazabilirim, ama biz burda hazi olan bir site üzerinden calistigim icin orada kalici degisiklik yapamiyorum Bu sebeple uzunluk her zaman 10 olarak kalicak. Bu sefer ben insanlar.length+1 üzerinden id verdigim icin id hep 11 olur. O zaman da key'de hata alirim ve warning alirim. Ama kendi API'miz üzerinden calisirken kalici olarak degisiklik yapabilecegimiz icin veriyi icerde yazabiliriz.
+
+  const myVeri = {
     id: insanlar.length + 1,
     name: faker.name.findName(),
     phone: faker.phone.phoneNumber(),
   };
 
-  console.log(veri); //Veri her tikladigimda bir sonraki veri olusut ama post'a bastigimda eklenir ve bir sonraki veri otomatik olusur.Yani posta basmadan aslinda veri olusuyor. Bastiktan sonra o veri ekleniyor ve yeni bir veri otomatik olarak olusuyor.Yani önce veri olusuyor eger post butonuna basarsam data arraya'ine eklenir.
+  console.log(myVeri); //Veri her tikladigimda bir sonraki veri olusut ama post'a bastigimda eklenir ve bir sonraki veri otomatik olusur.Yani posta basmadan aslinda veri olusuyor. Bastiktan sonra o veri ekleniyor ve yeni bir veri otomatik olarak olusuyor.Yani önce veri olusuyor eger post butonuna basarsam data arraya'ine eklenir.
   return (
     <div className="container text-center mt-4">
       <button
