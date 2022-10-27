9fc9a27f10c21d165c4fc85ca2e745b94a2854a7
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-10-27 11:47:51 +0200

    isActive with NavLink added

--- DE-08-React-Router/src/App.js
+++ DE-08-React-Router/src/App.js
@@ -10,12 +10,13 @@ import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
-    //Her zaman gelecek olanlar BrowserRouter altinda. Daha sonra tiklandiginda gelmesini istedigimiz componentler ise Routes kismi icerisinde. Yani cagirilan her bir componentte myNavbar ve Footer componentleri de gözüküyor olacak. Digerleri Cagirildikca gelir(Route ile cagiriyoruz.)
+    //!!!Her zaman gelecek olanlar BrowserRouter altinda. Daha sonra tiklandiginda gelmesini istedigimiz componentler ise Routes kismi icerisinde. Yani cagirilan her bir componentte myNavbar ve Footer componentleri de gözüküyor olacak. Digerleri Cagirildikca gelir(Route ile cagiriyoruz.)
+    //exact yazmamizin sebebi ana page oldugunu belli etmek.Eger onu en asagi yazarsak tek tek diger sayfalari kontrol ederken zaman kaybi olmasin diye onun ana yüklenecek sayfa oldugunu belirttik ve direk ona gitti
     <div>
       <BrowserRouter>
         <MyNavbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route exact path="/" element={<Home />} />
           <Route path="/contact" element={<ContactForm />} />
           <Route path="/courses" element={<CourseCard />} />
           <Route path="/teacher" element={<Teacher />} />
--- DE-08-React-Router/src/components/MyNavbar.jsx
+++ DE-08-React-Router/src/components/MyNavbar.jsx
@@ -3,6 +3,7 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../img/logo.png";
 import { Link, NavLink } from "react-router-dom";
 //!react-bootstrap MyNavbar component
+//!!!className="nav-link" vermemin sebebi normalde bunlar Nav.Link reactbootstrapti. Ama biz onu router ile kullanmak icin NavLink yaptik dolayisiyla styller kayboldu.Ama normal bootstrap kullanarak o stilleri geri cagirdik
 const MyNavbar = () => {
   return (
     <Navbar bg="light" expand="lg">
@@ -18,10 +19,42 @@ const MyNavbar = () => {
             style={{ maxHeight: "100px" }}
             navbarScroll
           >
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/teacher">Teacher</NavLink>
-            <NavLink to="/courses">Courses</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
+            <NavLink
+              style={({ isActive }) =>
+                isActive ? { color: "red" } : undefined
+              }
+              className="nav-link"
+              to="/"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              className="nav-link"
+              to="/teacher"
+              style={({ isActive }) =>
+                isActive ? { color: "red" } : undefined
+              }
+            >
+              Teacher
+            </NavLink>
+            <NavLink
+              className="nav-link"
+              to="/courses"
+              style={({ isActive }) =>
+                isActive ? { color: "red" } : undefined
+              }
+            >
+              Courses
+            </NavLink>
+            <NavLink
+              className="nav-link"
+              to="/contact"
+              style={({ isActive }) =>
+                isActive ? { color: "red" } : undefined
+              }
+            >
+              Contact
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
