dcea1bb0eb67b01a23b053357ca92928bd01656e
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-11-01 11:51:15 +0100

    header completed

---
+++ DE-09-styledComponentExample/src/components/styles/ContainerSSS.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const ContainerSSS = styled.div`
+  background-color: lightgray;
+  box-sizing: border-box;
+  padding: 0 20px;
+  max-width: 100%;
+  width: 1500px;
+`;
+
+export default ContainerSSS;
