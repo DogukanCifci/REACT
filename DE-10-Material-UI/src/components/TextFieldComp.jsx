99da498bda62ec177c078bdb5a82c876f142c708
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-11-02 11:42:21 +0100

    input types added(UI Material)

--- DE-10-Material-UI/src/components/TextFieldComp.jsx
+++ DE-10-Material-UI/src/components/TextFieldComp.jsx
@@ -1,7 +1,34 @@
+import { Box, Container, TextField, Typography } from "@mui/material";
+
 import React from "react";
 
 const TextFieldComp = () => {
-  return <div></div>;
+  let errorCheck = true;
+  return (
+    <div>
+      <Container maxWidth="lg">
+        <Typography>TEXT-FIELD</Typography>
+        <Box sx={{ mt: 5 }} align="center">
+          <TextField
+            id="email"
+            type="email"
+            fullWidth
+            margin="dense"
+            placeholder="Hello World"
+            defaultValue="Hello World"
+          />
+          <TextField
+            error={errorCheck}
+            id="password"
+            type="password"
+            align="center"
+            label="Password"
+            helperText={errorCheck ? "Yanlis Girdiniz" : "Correct"}
+          />
+        </Box>
+      </Container>
+    </div>
+  );
 };
 
 export default TextFieldComp;
