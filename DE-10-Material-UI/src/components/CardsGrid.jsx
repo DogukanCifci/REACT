880cb8a16b7a1a3ce8c75e841fa18d51cd3dfa16
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-11-02 11:59:29 +0100

    card-grid with UI Materials added

--- DE-10-Material-UI/src/components/CardsGrid.jsx
+++ DE-10-Material-UI/src/components/CardsGrid.jsx
@@ -1,7 +1,54 @@
+import {
+  Container,
+  Grid,
+  Typography,
+  Card,
+  CardContent,
+  CardActionArea,
+  CardMedia,
+  Button,
+  CardActions,
+} from "@mui/material";
 import React from "react";
-
+import MyData from "../data.js";
 const CardsGrid = () => {
-  return <div></div>;
+  return (
+    <Container sx={{ mt: 5 }}>
+      <Typography>UI Material CARD - GRID</Typography>
+      <Grid container spacing={10} alignItems="center" justifyContent="center">
+        {MyData.map((e) => {
+          const { id, name, text, img } = e;
+          return (
+            <Grid item key={id}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={img}
+                    alt="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {text}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Share
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Container>
+  );
 };
 
 export default CardsGrid;
