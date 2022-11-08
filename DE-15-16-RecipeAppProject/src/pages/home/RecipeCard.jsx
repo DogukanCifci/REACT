51e9776f32fa0626c740e50bacfb07ccfec5bdc1
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-11-08 09:42:43 +0100

    view details completed

--- DE-15-16-RecipeAppProject/src/pages/home/RecipeCard.jsx
+++ DE-15-16-RecipeAppProject/src/pages/home/RecipeCard.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { Button, Cards, RecipeHeader, RecipeImage } from "./HomeStyles";
+import { useNavigate } from "react-router-dom";
 
 const RecipeCard = ({ element }) => {
   const { label, image } = element.recipe;
+  const navigate = useNavigate();
   return (
     <Cards>
       <RecipeHeader>{label}</RecipeHeader>
       <RecipeImage src={image} alt="" />
-      <Button>View Details</Button>
+      <Button
+        onClick={() => {
+          navigate("/details", { state: { element } });
+        }}
+      >
+        View Details
+      </Button>
     </Cards>
   );
 };
