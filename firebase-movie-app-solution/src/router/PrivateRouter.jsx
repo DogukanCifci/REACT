23385438a774f0dc54c35537569fbcbd6df8299e
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-11-14 11:54:20 +0100

    private router added

---
+++ firebase-movie-app-solution/src/router/PrivateRouter.jsx
@@ -0,0 +1,9 @@
+import React, { useContext } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { AuthContext } from "../context/AuthContextProvider";
+
+const PrivateRouter = () => {
+  const { currentUser } = useContext(AuthContext);
+  return <div>{currentUser ? <Outlet /> : <Navigate to="/login" />}</div>;
+};
+export default PrivateRouter;
