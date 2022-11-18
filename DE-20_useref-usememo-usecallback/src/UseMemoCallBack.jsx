5b0e78de419bcbfa98c1ce8d2c1983ad7e9dd5e9
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-11-18 11:31:22 +0100

    usememoCallBack added

--- DE-20_useref-usememo-usecallback/src/UseMemoCallBack.jsx
+++ DE-20_useref-usememo-usecallback/src/UseMemoCallBack.jsx
@@ -18,6 +18,7 @@ const UseMemoCallBack = () => {
       .then((res) => setKullanicilar(res.data));
   }, []);
 
+  //Eger kullanicilari [] icine eklemezsem sadece lastSearch degistiginde burasi calisacagi icin sayfa ilk yüklendiginde bos gözükür. Ondan dolayi [] icine kullanicilari da ekliyoruz. Cünkü kullancilar sayfa ilk yüklendiginde render edildigi icin bos gözükmez.
   const filteredUsers = useMemo(
     () =>
       kullanicilar.filter((i) =>
@@ -26,12 +27,21 @@ const UseMemoCallBack = () => {
     [lastSearch, kullanicilar]
   );
 
+  const ekle = useCallback(() => {
+    setKullanicilar([
+      ...kullanicilar,
+      { id: kullanicilar.length + 1, name: "Osman" },
+    ]);
+  }, [kullanicilar]);
+
   return (
     <div>
       <input type="search" onChange={(e) => setText(e.target.value)} />
 
       <button onClick={() => setLastSearch(text)}>Search User</button>
-      <button id="add-button">Add User</button>
+      <button id="add-button" onClick={ekle}>
+        Add User
+      </button>
       {/*  <Users kullanicilar={kullanicilar} /> */}
       <Users kullanicilar={filteredUsers} />
     </div>
